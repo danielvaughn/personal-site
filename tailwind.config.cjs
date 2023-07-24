d1cda205786837c5597529a221764774cdb82f4f
Daniel Vaughn <danielbvaughn@gmail.com>
2023-07-23 22:49:31 -0400

    adds initial content for index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	fontFamily: {
+		sans: [
+			'Inter var, sans-serif',
+		],
+	},
+		extend: {
+			colors: {
+				'hackernews': '#ff4000',
+				'github': '#333',
+				'linkedin': '#0a66c2',
+				'twitter': '#1da1f2',
+			},
+		},
 	},
 	plugins: [],
 }
