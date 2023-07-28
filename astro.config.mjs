06a7981b3174c80a7e09ed9af92ea6c89c23e1a3
Daniel Vaughn <danielbvaughn@gmail.com>
2023-07-27 21:07:37 -0400

    adds vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/edge';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
